8ce98aa8539526969da95fef8da47ad3cb992f41
Roms1383 <romain.kelifa@gmail.com>
2022-02-20 20:26:55 +0700

    Fix enum test
    Fix enum_test in dart_example

--- dart_example/test/enum_test.dart
+++ dart_example/test/enum_test.dart
@@ -5,9 +5,9 @@ import 'package:dart_example/locations.dart';
 void main() {
   test('can handle a class enum', () async {
     final accounts = AccountsApi();
-    expect((await accounts.enumReturn(status: StatusActiveItem())),
-        equals(StatusActiveItem()));
-    expect((await accounts.enumReturn(status: StatusPendingItem())),
-        isNot(equals(StatusActiveItem())));
+    expect((await accounts.enumReturn(status: Status.active)),
+        equals(Status.active));
+    expect((await accounts.enumReturn(status: Status.active)),
+        isNot(equals(Status.pending)));
   });
 }
