057ce9f32cee4bee255c707eda5ed001cebdd71e
jerel <jerel.unruh@toyotaconnected.com>
2022-06-03 14:33:32 -0500

    Use pthread_self instead of syscall

--- example/c/threading_example.c
+++ example/c/threading_example.c
@@ -3,12 +3,11 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <threading_example.h>
-#include <sys/syscall.h>
 
 void init(MembraneHandle *handle)
 {
     setbuf(stdout, NULL);
-    pthread_t thread_id;
+    pthread_t thread_id = 0;
 
     printf("\n[call_async_c] [C] Spawning detached thread\n");
     pthread_detach(thread_id);
@@ -39,20 +38,20 @@ void *worker(void *vptr)
 
     int i = 0;
     char buffer[100];
-    pid_t tid = syscall(SYS_gettid);
+    unsigned long tid = (unsigned long)pthread_self();
 
     usleep(5000);
-    printf("\n[call_async_c] [C] This is running in detached C thread %d after sleeping for 5ms \n", tid);
+    printf("\n[call_async_c] [C] This is running in detached C thread %lu after sleeping for 5ms \n", tid);
 
     while (!handle.is_done(handle.is_done_ctx))
     {
         i++;
-        sprintf(buffer, "This is a string from a C thread: Thread %d, Count %d", tid, i);
+        sprintf(buffer, "This is a string from a C thread: Thread %lu, Count %d", tid, i);
         handle.push(handle.push_ctx, buffer);
         printf("[call_async_c] [C] %s\n", buffer);
         usleep(50);
     }
-    printf("[call_async_c] [C] stream was closed, Thread %d shutting down\n", tid);
+    printf("[call_async_c] [C] stream was closed, Thread %lu shutting down\n", tid);
 
     pthread_exit(NULL);
 }
